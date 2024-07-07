0cb5b0483d5a7af244bff18d574350ef67d8cd95
Walter kibet <kibetwalter726@gmail.com>
2024-07-07 11:53:27 +0300

    update

--- KeDigital/settings.py
+++ KeDigital/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    # 'debug_toolbar',
     'django_extensions',
     'easy_thumbnails',
     'social_django',
@@ -128,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGIN_REDIRECT_URL = 'dashboard'
+# LOGIN_URL = 'login'
+# LOGOUT_URL = 'logout
+
--- account/models.py
+++ account/models.py
@@ -2,3 +2,17 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(
+        upload_to='users/%Y/%m/%d/',
+        blank=True
+    )
+    def __str__(Self):
+        return f'Profile to {self.user.username}'
+
+        
