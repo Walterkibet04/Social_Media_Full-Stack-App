60d462bba359bfaded1fb692b294916504bbe8d8
Walter kibet <kibetwalter726@gmail.com>
2024-07-09 23:14:28 +0300

    update

--- images/views.py
+++ images/views.py
@@ -34,5 +34,58 @@ def image_create(request):
     )
 
 def image_detail(request, id, slug):
-    pass
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request,
+        'images/image/detail.html',
+        {'section': 'images', 'image': image},
+    )
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id) 
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images},
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images},
+    )
 
